8a3c0843bd012608be8c0c6cfa9a8b7ea2f6c014
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-04-04 16:31:23 +0100

    Complete test 5

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,17 @@
 const repeatString = function(string, num) {
     let repeatString = "";
+
+    if (num < 0) {
+        repeatString = "ERROR";
+    }
+
     for (i = 0; i < num; i++) {
         repeatString += string;
     }
     return repeatString;
 };
 
-repeatString("hey", 0);
+repeatString("hey", -1);
 
 // Do not edit below this line
 module.exports = repeatString;
