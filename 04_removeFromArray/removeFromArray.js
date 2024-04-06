e0a8ee62c4113a1426868f23e20ef6da43735740
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-04-06 14:19:52 +0100

    Complete test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
+const numberArray = [1, 2, 3, 4, 5];
 
+const removeFromArray = function(array, num) {
+    let filteredArray = array.filter(item => item !== num);
+    return filteredArray;
 };
+
+removeFromArray(numberArray, 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
