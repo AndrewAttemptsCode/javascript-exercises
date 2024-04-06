8f2c7509cb62ebe49dc1b34525dffe770882bdf2
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-04-06 13:05:24 +0100

    Complete test 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let string = "";
+    for (let i = text.length - 1; i >= 0; i--) {
+        string += text[i];
+    }
+    return string;
 };
+
+reverseString("test");
 
 // Do not edit below this line
 module.exports = reverseString;
