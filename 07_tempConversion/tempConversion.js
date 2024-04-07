8981de5a0f0a1c81656dccc1e948c2aa467141fb
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-04-07 16:25:49 +0100

    Complete test 2

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,14 @@
 const convertToCelsius = function(temp) {
   // °C = (°F - 32) × 5/9
   let celsius = (temp - 32) * 5 / 9;
-  return celsius;
+  let roundCelcius = Math.round(celsius * 10) / 10
+  return roundCelcius;
 };
 
 const convertToFahrenheit = function(temp) {
 };
 
-convertToCelsius(32);
+convertToCelsius(100);
 // convertToFahrenheit(0);
 
 // Do not edit below this line
