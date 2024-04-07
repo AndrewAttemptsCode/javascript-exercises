2a85b8211030a307f552870ea91a3c4506bd9403
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-04-07 15:18:54 +0100

    Complete test 4

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,16 @@
 const leapYears = function(year) {
-    if (year % 4 === 0 || year % 400 === 0){
+    if (year % 400 === 0){
+        return true;
+    } else if (year % 100 === 0) {
+        return false;
+    } else if (year % 4 === 0){
         return true;
     } else {
         return false;
     }
 };
 
-leapYears(34992);
+leapYears(1900);
 
 // Do not edit below this line
 module.exports = leapYears;
