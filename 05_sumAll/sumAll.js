aab819372c4dbf6f7b0a48c9d26078866aadfa88
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-04-07 13:59:45 +0100

    Complete test 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,8 @@
 const sumAll = function(num1, num2) {
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    }
+    
     if (num1 < 0 || num2 < 0) {
         return "ERROR";
     }
@@ -16,7 +20,7 @@ const sumAll = function(num1, num2) {
     return sum;
 };
 
-sumAll(-10, 4);
+sumAll(10, "90");
 
 // Do not edit below this line
 module.exports = sumAll;
