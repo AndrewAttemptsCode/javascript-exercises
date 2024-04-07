357f5073e983fb1731678810640aa18e2190de87
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-04-07 16:12:11 +0100

    Complete test 1

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // °C = (°F - 32) × 5/9
+  let celsius = (temp - 32) * 5 / 9;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
 };
+
+convertToCelsius(32);
+// convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {
