f1d0493e1ead61e58b9f5fff428f9569f9e8ca90
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-04-07 13:34:10 +0100

    Complete test 3

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,18 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
-    for (let i = num1; i <= num2; i++) {
-        sum += i; 
+    if (num1 > num2) {
+        for (let i = num1; i >= num2; i--) {
+            sum += i; 
+        }
+    } else {
+        for (let i = num1; i <= num2; i++) {
+            sum += i; 
+        }
     }
     return sum;
 };
 
-sumAll(1, 4000);
+sumAll(123, 1);
 
 // Do not edit below this line
 module.exports = sumAll;
