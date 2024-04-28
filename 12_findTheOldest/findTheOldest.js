47219b821ec47ae85444e3a08c5bcf2c54590179
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-04-28 15:51:23 +0100

    Complete exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const date = new Date().getFullYear();
 
+    return array.reduce((oldest, current) => {
+        const ageOldest = (oldest.yearOfDeath || date) - oldest.yearOfBirth;
+        const ageCurrent = (current.yearOfDeath || date) - current.yearOfBirth;
+        return ageOldest > ageCurrent ? oldest : current;
+    });
 };
 
 // Do not edit below this line
